d549bf0f4c0446adcec4354228e54c62180ab1a9
Josh Barnett <oh@heyjo.sh>
2020-08-09 01:24:05 -0400

    Trying to get audio to play
    Think there's an issue with SSR-first generation

--- components/Sound.js
+++ components/Sound.js
@@ -2,9 +2,7 @@ import React from "react";
 
 export default function Sound({ emoji, src }) {
   const play = src => {
-    const audio = new Audio(
-      "https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3"
-    );
+    const audio = new Audio(`/${src}.mp3`);
     audio.play();
   };
   return (
@@ -12,7 +10,7 @@ export default function Sound({ emoji, src }) {
       <button
         type="button"
         className="flex-1 items-center px-6 py-3 bg-gray-100 border-4 border-gray-600 text-4xl rounded-lg p-3 sm:p-4 md:p-5 lg:p-6 hover:border-blue-700 hover:bg-blue-600 focus:outline-none focus:shadow-outline focus:border-blue-700 focus:bg-blue-700 active:border-blue-700 active:bg-blue-700 transition ease-in-out duration-150"
-        onClick={play}
+        onClick={play(src)}
       >
         {emoji}
       </button>
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,25 @@
+import dynamic from "next/dynamic";
 import React, { Component } from "react";
-import Sound from "../components/Sound";
+
+const Sound = dynamic(() => import("../components/Sound"));
 
 export default class extends Component {
   render() {
     return (
       <main className="m-3 sm:m-4 md:m-5 lg:m-6">
-        <h1 class="text-gray-700 text-lg font-medium mb-3">
+        <h1 className="text-gray-700 text-lg font-medium mb-3">
           Husband Soundboard
         </h1>
         <ul className="grid grid-cols-3 gap-6 lg:grid-cols-5 items-center justify-center h-full">
-          <Sound emoji="❤️" src="I love you." />
-          <Sound emoji="👗" src="You look really nice today." />
-          <Sound
-            emoji="👶"
-            src="I've got the baby. Take some time for yourself."
-          />
-          <Sound emoji="🍽" src="I took care of the dishes already." />
-          <Sound emoji="🧐" src="You're right." />
-          <Sound
-            emoji="🌱"
-            src="That plant doesn't seen too expensive. You should get it."
-          />
-          <Sound emoji="🍱" src="I got a babysitter so we can go out." />
-          <Sound
-            emoji="🧺"
-            src="I got a head start on the laundry. I hope that's okay."
-          />
-          <Sound emoji="😔" src="I'm sorry." />
+          <Sound emoji="❤️" src="love" />
+          <Sound emoji="👗" src="look" />
+          <Sound emoji="👶" src="baby" />
+          <Sound emoji="🍽" src="dishes" />
+          <Sound emoji="🧐" src="right" />
+          <Sound emoji="🌱" src="plant" />
+          <Sound emoji="🍱" src="babysitter" />
+          <Sound emoji="🧺" src="laundry" />
+          <Sound emoji="😔" src="sorry" />
         </ul>
       </main>
     );
