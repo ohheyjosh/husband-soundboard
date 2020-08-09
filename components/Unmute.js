11ba6c4c7df683af92f54269c067d33fde2ab336
Josh Barnett <oh@heyjo.sh>
2020-08-09 09:15:23 -0400

    Extracted unmute to separate component

---
+++ components/Unmute.js
@@ -0,0 +1,7 @@
+import unmuteAudio from "unmute-ios-audio";
+
+unmuteAudio();
+
+export default function Unmute() {
+  return null;
+}
--- pages/index.js
+++ pages/index.js
@@ -2,44 +2,45 @@ import React, { Component } from "react";
 import dynamic from "next/dynamic";
 import Sound from "../components/Sound";
 
-const unmuteAudio = dynamic(
+const Unmute = dynamic(
   () => {
-    return import("unmute-ios-audio");
+    return import("../components/Unmute");
   },
   { ssr: false }
 );
 
 export default class extends Component {
   render() {
-    // Call once to circumvent iOS's muting behavior
-    unmuteAudio();
-
     return (
-      <main className="m-3 sm:m-4 md:m-5 lg:m-6">
-        <h1 className="text-gray-700 text-lg font-medium mb-4 sm:mb-6">
-          Husband Soundboard
-        </h1>
-        <ul className="grid grid-cols-3 gap-6 lg:grid-cols-5 items-center justify-center h-full">
-          <Sound emoji="❤️" src="/love.mp3" />
-          <Sound emoji="👗" src="/look.mp3" />
-          <Sound emoji="👶" src="/baby.mp3" />
-          <Sound emoji="🍽" src="/dishes.mp3" />
-          <Sound emoji="🧐" src="/right.mp3" />
-          <Sound emoji="🌱" src="/plant.mp3" />
-          <Sound emoji="🍱" src="/babysitter.mp3" />
-          <Sound emoji="🧺" src="/laundry.mp3" />
-          <Sound emoji="😔" src="/sorry.mp3" />
-        </ul>
-        <aside className="text-gray-500 text-base font-medium text-right mt-4 sm:mt-6">
-          made with ♥ by{" "}
-          <a
-            href="https://github.com/ohheyjosh"
-            className="text-gray-700 underline"
-          >
-            this husband
-          </a>
-        </aside>
-      </main>
+      <>
+        {/* Call once to circumvent iOS's muting behavior */}
+        <Unmute />
+        <main className="m-3 sm:m-4 md:m-5 lg:m-6">
+          <h1 className="text-gray-700 text-lg font-medium mb-4 sm:mb-6">
+            Husband Soundboard
+          </h1>
+          <ul className="grid grid-cols-3 gap-6 lg:grid-cols-5 items-center justify-center h-full">
+            <Sound emoji="❤️" src="/love.mp3" />
+            <Sound emoji="👗" src="/look.mp3" />
+            <Sound emoji="👶" src="/baby.mp3" />
+            <Sound emoji="🍽" src="/dishes.mp3" />
+            <Sound emoji="🧐" src="/right.mp3" />
+            <Sound emoji="🌱" src="/plant.mp3" />
+            <Sound emoji="🍱" src="/babysitter.mp3" />
+            <Sound emoji="🧺" src="/laundry.mp3" />
+            <Sound emoji="😔" src="/sorry.mp3" />
+          </ul>
+          <aside className="text-gray-500 text-base font-medium text-right mt-4 sm:mt-6">
+            made with ♥ by{" "}
+            <a
+              href="https://github.com/ohheyjosh"
+              className="text-gray-700 underline"
+            >
+              this husband
+            </a>
+          </aside>
+        </main>
+      </>
     );
   }
 }
