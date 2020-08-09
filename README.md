262371bfe9e8a6c45a695b4fdf37d10129298690
Josh Barnett <oh@heyjo.sh>
2020-08-09 02:25:09 -0400

    🚢

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # husband-soundboard
+
 Giving my partner the soundbites she needs when she needs them.
+
+![Screenshot](screenshot.png)
+
+#### Built with:
+
+- [React](https://github.com/facebook/react)
+- [Next.js](https://github.com/vercel/next.js)
+- [PostCSS](https://github.com/postcss/postcss)
+- [TailwindCSS](https://github.com/tailwindlabs/tailwindcss)
+- [Howler](https://github.com/goldfire/howler.js) via [use-sound](https://github.com/joshwcomeau/use-sound)
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default class extends Component {
   render() {
     return (
       <main className="m-3 sm:m-4 md:m-5 lg:m-6">
-        <h1 className="text-gray-700 text-lg font-medium mb-3">
+        <h1 className="text-gray-700 text-lg font-medium mb-4 sm:mb-6">
           Husband Soundboard
         </h1>
         <ul className="grid grid-cols-3 gap-6 lg:grid-cols-5 items-center justify-center h-full">
@@ -19,6 +19,15 @@ export default class extends Component {
           <Sound emoji="🧺" src="/laundry.mp3" />
           <Sound emoji="😔" src="/sorry.mp3" />
         </ul>
+        <aside className="text-gray-500 text-base font-medium text-right mt-4 sm:mt-6">
+          made with ♥ by{" "}
+          <a
+            href="https://github.com/ohheyjosh"
+            className="text-gray-700 underline"
+          >
+            this husband
+          </a>
+        </aside>
       </main>
     );
   }
