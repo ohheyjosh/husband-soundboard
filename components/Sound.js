9b13700306e27731e062bb383b91813261b70862
Josh Barnett <oh@heyjo.sh>
2020-08-09 00:41:11 -0400

    Styled static buttons in

--- components/Sound.js
+++ components/Sound.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function Sound({ src }) {
-  return <>👋</>;
+export default function Sound({ emoji }) {
+  return (
+    <li className="col-span-1 flex rounded-md shadow-md hover:shadow-lg transform hover:-translate-y-1 ease-in-out duration-300">
+      <button
+        type="button"
+        className="flex-1 items-center px-6 py-3 bg-gray-100 border-4 border-gray-600 text-4xl rounded-lg p-3 sm:p-4 md:p-5 lg:p-6 hover:border-blue-700 hover:bg-blue-600 focus:outline-none focus:shadow-outline focus:border-blue-700 focus:bg-blue-700 active:border-blue-700 active:bg-blue-700 transition ease-in-out duration-150"
+      >
+        {emoji}
+      </button>
+    </li>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,32 @@ import Sound from "../components/Sound";
 
 export default class extends Component {
   render() {
-    return <Sound />;
+    return (
+      <main className="m-3 sm:m-4 md:m-5 lg:m-6">
+        <h1 class="text-gray-700 text-lg font-medium mb-3">
+          Husband Soundboard
+        </h1>
+        <ul className="grid grid-cols-3 gap-6 lg:grid-cols-5 items-center justify-center h-full">
+          <Sound emoji="❤️" src="I love you." />
+          <Sound emoji="👗" src="You look really nice today." />
+          <Sound
+            emoji="👶"
+            src="I've got the baby. Take some time for yourself."
+          />
+          <Sound emoji="🍽" src="I took care of the dishes already." />
+          <Sound emoji="🧐" src="You're right." />
+          <Sound
+            emoji="🌱"
+            src="That plant doesn't seen too expensive. You should get it."
+          />
+          <Sound emoji="🍱" src="I got a babysitter so we can go out." />
+          <Sound
+            emoji="🧺"
+            src="I got a head start on the laundry. I hope that's okay."
+          />
+          <Sound emoji="😔" src="I'm sorry." />
+        </ul>
+      </main>
+    );
   }
 }
